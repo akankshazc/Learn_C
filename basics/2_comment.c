cdd373ef82b9f3298e9f6ae2152478b26ccf7541
Akanksha <akankshazc23@icloud.com>
2024-12-17 19:50:47 +0530

    Comments

---
+++ basics/2_comment.c
@@ -0,0 +1,16 @@
+// Program to illustrate
+// use of comments
+#include <stdio.h>
+
+int main(void)
+{
+    // This is a single-line comment
+
+    /*
+    This is also a
+    multi-line comment
+    */
+
+    printf("Welcome to Learning C");
+    return 0;
+}
