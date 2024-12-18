3b36fb639d3d80a35b857509adcd06f23baaed36
Akanksha <akankshazc23@icloud.com>
2024-12-18 06:44:00 +0530

    added comments and alternate ways to display.

--- basics/1_first_program.c
+++ basics/1_first_program.c
@@ -1,8 +1,31 @@
+// Program to display "Hello, World!"
+
+// Header file for input output functions
 #include <stdio.h>
 
+// Main function: entry point for execution
 int main()
 {
+    // the print statement
     printf("Hello, World!\n");
+
+    /*
+    Other ways to write a "Hello, World!" program:
+    */
+
+    // Using puts() function
+    puts("Hello, World!");
+
+    // Using write() system call
+    // Generates a warning (less portable and more advanced)
+    write(1, "Hello, World!\n", 14);
+
+    // Using putchar() function
+    char *str = "Hello, World!\n";
+    while (*str)
+    {
+        putchar(*str++);
+    }
 
     return 0;
 }
