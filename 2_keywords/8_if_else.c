a6fe5a7e5c7ebbed00749aea362d71575e276c16
Akanksha <akankshazc23@icloud.com>
2024-12-19 22:55:07 +0530

    do-while keywords and if-else keywords (loops)

---
+++ 2_keywords/8_if_else.c
@@ -0,0 +1,18 @@
+// Program to demonstrate if-else keyword (and if-else statement)
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    if (i > 10)
+    {
+        printf("i is greater than 10\n");
+    }
+    else
+    {
+        printf("i is less than or equal to 10\n");
+    }
+
+    return 0;
+}
