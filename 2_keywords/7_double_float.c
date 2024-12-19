3cb91b52060b5d608e5e7546d763317f3792aa92
Akanksha <akankshazc23@icloud.com>
2024-12-19 22:55:20 +0530

    keywords: double, float

---
+++ 2_keywords/7_double_float.c
@@ -0,0 +1,12 @@
+// Program to demonstrate double, float keyword
+
+#include <stdio.h>
+
+int main()
+{
+    float f = 0.3;
+    double d = 10.67;
+    printf("The value of f is %f\n", f);
+    printf("The value of d is %lf\n", d);
+    return 0;
+}
