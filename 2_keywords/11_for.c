27d7e078d1bab30179fd95ac6bdaccb68350cfad
Akanksha <akankshazc23@icloud.com>
2024-12-20 23:27:58 +0530

    for loop and while loop

---
+++ 2_keywords/11_for.c
@@ -0,0 +1,13 @@
+// Program to demonstrate for keyword (and for loop)
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
---
+++ 2_keywords/18_while.c
@@ -0,0 +1,18 @@
+// Program to demonstrate while keyword
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    int i = 0;
+
+    // while loop that executes till i is less than 5
+    while (i < 5)
+    {
+        printf("Value of i: %d\n", i);
+        i++;
+    }
+
+    return 0;
+}
