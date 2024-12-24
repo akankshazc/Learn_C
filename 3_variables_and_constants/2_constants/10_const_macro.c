a12f326ffb851ae9b2878643a480a029382a3283
Akanksha <akankshazc23@icloud.com>
2024-12-24 21:04:38 +0530

    constants using define

---
+++ 3_variables_and_constants/2_constants/10_const_macro.c
@@ -0,0 +1,13 @@
+// Program to define a constant using #define
+
+#include <stdio.h>
+
+// defining a constant using #define
+#define PI 3.14159
+
+// main function
+int main()
+{
+    printf("Value of PI: %f\n", PI);
+    return 0;
+}
