37740d57b9a8b07e160e0cc0259385f6769bb44d
Akanksha <akankshazc23@icloud.com>
2024-12-26 21:08:18 +0530

    Add example program for double data type

---
+++ 4_data_types/1_primitive_types/4_double.c
@@ -0,0 +1,17 @@
+// Program to demonstrate use of double data type
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    double a = 123123123.00;
+    double b = 123.123123;
+    double c = 123123123123.123132;
+
+    printf("Value of a: %lf\n", a);
+    printf("Value of b: %lf\n", b);
+    printf("Value of c: %lf\n", c);
+
+    return 0;
+}
