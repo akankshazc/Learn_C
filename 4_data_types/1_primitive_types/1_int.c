f77ea7f6f9e907f0b6f92262c3a9b39c53cbcf99
Akanksha <akankshazc23@icloud.com>
2024-12-26 21:07:56 +0530

    int data type

---
+++ 4_data_types/1_primitive_types/1_int.c
@@ -0,0 +1,25 @@
+// Program to demonstrate Integer data type
+
+#include <stdio.h>
+
+int main()
+{
+    // Integer value with positive data
+    int a = 10;
+
+    // Integer value with negative data
+    int b = -10;
+
+    // U or u used for unsigned int
+    int c = 89U;
+
+    // L or l used for long int
+    int d = 100987L;
+
+    printf("Integer value with positive data: %d\n", a);
+    printf("Integer value with negative data: %d\n", b);
+    printf("Unsigned Integer value: %d\n", c);
+    printf("Long Integer value: %d\n", d);
+
+    return 0;
+}
