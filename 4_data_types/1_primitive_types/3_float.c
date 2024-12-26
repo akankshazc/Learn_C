302df7ef67e2a5a2dd19d8ade9f31708515d7b88
Akanksha <akankshazc23@icloud.com>
2024-12-26 21:08:12 +0530

    Add example program for Float data type

---
+++ 4_data_types/1_primitive_types/3_float.c
@@ -0,0 +1,19 @@
+// Program to demonstrate Float data type
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    float a = 9.0f;
+    float b = 2.5f;
+
+    // 2X10^-4
+    float c = 2e-4f;
+
+    printf("Value of a: %f\n", a);
+    printf("Value of b: %f\n", b);
+    printf("Value of c: %f\n", c);
+
+    return 0;
+}
