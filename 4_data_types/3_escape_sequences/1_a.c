04865844e86f8eca872f0386d4b001df27197869
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:11:10 +0530

    Add example program to illustrate \a escape sequence

---
+++ 4_data_types/3_escape_sequences/1_a.c
@@ -0,0 +1,12 @@
+// C program to illustrate \a escape sequence
+
+#include <stdio.h>
+
+int main()
+{
+    // output may depend on the compiler
+    printf("ID:"
+           "1\a2\a3\a4\a5\a6\a7\a8\a9\a0\a");
+
+    return 0;
+}
