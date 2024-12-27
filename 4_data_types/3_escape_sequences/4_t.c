b3d7a8bb09f34f542ddb262ba9a314d1431d06e2
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:11:30 +0530

    Add example program to demonstrate \t escape sequence

---
+++ 4_data_types/3_escape_sequences/4_t.c
@@ -0,0 +1,12 @@
+// Program to demonstrate \t escape sequence
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // \t - horizontal tab character
+    printf("Hello\tWorld\n");
+
+    return 0;
+}
