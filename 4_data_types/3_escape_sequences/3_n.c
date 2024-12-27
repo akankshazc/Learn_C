f4751e7eef59e5c3acb488c61c3f18f60a659196
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:11:24 +0530

    Add example program to demonstrate \n escape sequence

---
+++ 4_data_types/3_escape_sequences/3_n.c
@@ -0,0 +1,12 @@
+// Program to demonstrate \n escape sequence
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // \n - new line character
+    printf("Hello\nWorld\n");
+
+    return 0;
+}
