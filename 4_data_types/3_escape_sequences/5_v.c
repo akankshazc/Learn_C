aba47a8520b8747e814b18b9029824783b6896a4
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:11:36 +0530

    Add example program to demonstrate \v escape sequence

---
+++ 4_data_types/3_escape_sequences/5_v.c
@@ -0,0 +1,12 @@
+// Program to demonstrate \v escape sequence
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // \v - vertical tab character
+    printf("Hello\vWorld\n");
+
+    return 0;
+}
