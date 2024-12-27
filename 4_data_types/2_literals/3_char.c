911e7360a0989c394399fcb78100f02c60f231e2
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:10:21 +0530

    Add example program to demonstrate character literals

---
+++ 4_data_types/2_literals/3_char.c
@@ -0,0 +1,14 @@
+// Program to demonstrate character literals
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // constant character literal
+    const char charVal = 'a';
+
+    printf("Character literal: %c\n", charVal);
+
+    return 0;
+}
