40871a10b080c5a89ca0640a2ee6d7a2e6e80860
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:11:18 +0530

    Add example program to demonstrate \b escape sequence

---
+++ 4_data_types/3_escape_sequences/2_b.c
@@ -0,0 +1,13 @@
+// Program to demonstrate \b escape sequence
+
+#include <stdio.h>
+
+int main()
+{
+    // \b - backspace character transfers
+    // the cursor one character back with
+    // or without deleting the character
+    printf("Hello \b\b\b\b\b\bWorld");
+
+    return 0;
+}
