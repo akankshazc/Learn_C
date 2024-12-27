e96fc4fdaeb3c3ed6d24814526bd1c4f369f9c1a
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:10:08 +0530

    Add example program to demonstrate integer literals

---
+++ 4_data_types/2_literals/1_int.c
@@ -0,0 +1,14 @@
+// Program to demonstrate interger literals
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // constant integer literal
+    const int intVal = 10;
+
+    printf("Integer literal: %d\n", intVal);
+
+    return 0;
+}
