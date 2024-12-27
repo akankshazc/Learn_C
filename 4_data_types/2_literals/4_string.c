ed7fa560d5bee3f1775e72a1fd23d6b82aae5226
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:10:27 +0530

    Add example program to demonstrate string literals

---
+++ 4_data_types/2_literals/4_string.c
@@ -0,0 +1,12 @@
+// Program to demonstrate string literals
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    const char str[] = "The\nHitchhiker's\nGuide\nto\nthe\nGalaxy";
+    printf("String literal: %s\n", str);
+
+    return 0;
+}
