4a1f9aaf1674aaf4bd26c0ffd8749d306cc72637
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:10:14 +0530

    Add example program to demonstrate float literals

---
+++ 4_data_types/2_literals/2_float.c
@@ -0,0 +1,14 @@
+// Program to demonstrate float literals
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // constant float literal
+    const float floatVal = 10.59;
+
+    printf("Float literal: %.2f\n", floatVal);
+
+    return 0;
+}
