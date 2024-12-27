ec1d794e26138c4949719db5d2d084c17ff863bd
Akanksha <akankshazc23@icloud.com>
2024-12-27 22:12:00 +0530

    Add example program to illustrate \r escape sequence

---
+++ 4_data_types/3_escape_sequences/6_r.c
@@ -0,0 +1,12 @@
+// Program to illustrate \r escape sequence
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // \r - carriage return character
+    printf("Hello world\rWorld\n");
+
+    return 0;
+}
