0cfac3be60a93d11a410a1f7c7ad6bc4f35ab789
Akanksha <akankshazc23@icloud.com>
2024-12-28 22:58:40 +0530

    Add example program to demonstrate \' and \" escape sequences

---
+++ 4_data_types/3_escape_sequences/8_quote.c
@@ -0,0 +1,12 @@
+// Program to demonstrate use of \' and \" escape sequences
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    printf("\' Hello world\n");
+    printf("\" Hello world\n");
+
+    return 0;
+}
