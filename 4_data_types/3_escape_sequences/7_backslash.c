091686da053248fe28b04bffe8e646772baad965
Akanksha <akankshazc23@icloud.com>
2024-12-28 22:58:34 +0530

    Add example program to demonstrate \\ escape sequence

---
+++ 4_data_types/3_escape_sequences/7_backslash.c
@@ -0,0 +1,13 @@
+// Program to demonstrate \\ escape sequence
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    // printing the string
+    printf("Hello, \\World! \\n");
+
+    // returning 0 from main
+    return 0;
+}
