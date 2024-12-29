ccde7f1b7b3423c46c2b525e66418c813b7a9f15
Akanksha <akankshazc23@icloud.com>
2024-12-29 22:06:56 +0530

    Add example program to demonstrate boolean data type using header file

---
+++ 4_data_types/4_bool/1_header.c
@@ -0,0 +1,18 @@
+// Program to implement boolean data type using header file
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// main function
+int main()
+{
+    // Boolean data types declared
+    bool a = true;
+    bool b = false;
+
+    // Print the boolean values as integers (1 for true and 0 for false)
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
