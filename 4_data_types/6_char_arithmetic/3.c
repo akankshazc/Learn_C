6c96f74d2a21149e0719cb2ae41340b38f32e636
Akanksha <akankshazc23@icloud.com>
2024-12-30 22:00:02 +0530

    Add example program to demonstrate character arithmetic

---
+++ 4_data_types/6_char_arithmetic/3.c
@@ -0,0 +1,16 @@
+// Program to demonstrate character arithmetic
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    char a = 'A';
+    char b = 'B';
+
+    // character arithmetic
+    printf("a + b = %d\n", a + b);
+    printf("a + b = %c\n", a + b);
+
+    return 0;
+}
