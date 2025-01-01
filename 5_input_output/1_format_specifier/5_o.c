1b33b434bcff6735ab6d264cc5371df5a8f82671
Akanksha <akankshazc23@icloud.com>
2025-01-01 21:37:40 +0530

    Add example program to demonstrate use of %o format specifier

---
+++ 5_input_output/1_format_specifier/5_o.c
@@ -0,0 +1,12 @@
+// Program to demonstrate use of %o format specifier
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+    int a = 67;
+    printf("Value of a = %o\n", a);
+
+    return 0;
+}
