093d8758608980e7f3db6328745536f179b239ae
Akanksha <akankshazc23@icloud.com>
2025-01-23 23:16:30 +0530

    Add example program to demonstrate use of %s format specifier

---
+++ 5_input_output/1_format_specifier/7_x.c
@@ -0,0 +1,10 @@
+// C progrma to demonstrate the use of %s format specifier
+
+#include <stdio.h>
+
+int main()
+{
+    char a[] = "Hello, World!";
+    printf("Value of a = %s\n", a);
+    return 0;
+}
